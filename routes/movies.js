ec58e4a821c1a729e5e2f890f9a22b357ca74f4c
Jonathan Bridge <jvbridge@gmail.com>
2022-04-29 15:32:36 -0700

    added the get and post path

--- routes/movies.js
+++ routes/movies.js
@@ -1,21 +1,50 @@
 const movies = require('express').Router();
 const mysql = require('mysql2');
 
+const db = mysql.createConnection(
+    {
+    host:"127.0.0.1",
+    user: "root",
+    password: "password",
+    database: 'movie_db'
+    },
+    console.log("connected to the database!")
+);
+
 // get route for basic movies
 movies.get('/', (req, res) =>{
     console.info("got a GET request for movies");
-    res.json("All movies will be returned through this route");
+
+    db.query("SELECT * FROM movies", (err, results) => {
+        if (err){
+            console.error("got an error!");
+            res.json(err);
+            return;
+        }
+        res.json(results);
+    });
+
 });
 
 // TODO: hook up DB
 
 movies.post('/add-movie', (req, res) => {
     console.info(req.body);
-    res.json("Still working on this, will post soon");
+    const movieName = req.body.name;
+    db.query("INSERT INTO movies(movie_name) VALUES(?)", movieName ,(err, results) =>{
+        if (err) console.error("We got an error inserting into the database");
+        res.json(`Adding movie: ${movieName}`);
+    });
+    
 });
 
 movies.post('/update-review', (req, res) => {
     console.info("got a post request to update a review");
+    const {text, id} = req.body;
+    db.query("INSERT INTO reviews(review, movie_id) VALUES(?)", [text, id], (err, results) =>{
+        if (err) console.error("We got an error inserting into the database");
+        res.json(`Adding review`);
+    });
     res.json("You tried to add a review");
 });
 
