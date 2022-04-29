452a9168c6927ce9bd94798ddf5a30299820bc9e
YiLin Ong <chauntelleinc@gmail.com>
2022-04-29 16:51:31 -0400

    added movie names and reviews on seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,27 @@
+INSERT INTO movies (name)
+VALUES ("Pirates of the Caribbean: The Curse of the Black Pearl"),
+       ("Up"),
+       ("Toy Story"),
+       ("Monsters, Inc."),
+       ("Finding Nemo"),
+       ("The Incredibles"),
+       ("WALL-E"),
+       ("Monsters University"),
+       ("The Addams Family"),
+       ("Coco");
+
+INSERT INTO reviews (name, movies)
+VALUES ("Finally, a real swashbuckling film to make me laugh", 1),
+       ("Proof that Disney can still create good movies", 1),
+       ("What i'm saying is I couldn't get enough of this movie. It was so well done.", 1),
+       ("Drag me to heaven", 2),
+       ("Best animated movies ever made", 2),
+       ("Created a decade ago, but still stunning", 3),
+       ("Witty Sweet Fun For All Ages. Forever Changed Animated Film", 4),
+       ("Just keep swimming..", 5),
+       ("Pixar Remains the Best Family Film Industry Out There Today...", 6),
+       ("A Most Endearing Love Story In Space...And Back", 7),
+       ("Pleasant Story of Friendship", 8),
+       ("The Addams Family- An amusing and stylish 90's revamp of the classic comedy franchise!", 9),
+       ("Childhood memories", 10),
+       ("Worth your time.", 7);
